570a1871f3c738beb34d6c9e213832a0ac7843c4
chirrylun <134626109+chirrylun@users.noreply.github.com>
2024-08-17 12:53:38 +0100

    Update page.js

--- react-dashboard/app/page.js
+++ react-dashboard/app/page.js
@@ -5,8 +5,9 @@ export default function Home() {
     <main className="flex">
       <div className="bg-primary max-w-xs w-full h-screen"></div>
       <div className="block h-screen w-full">
-        <div className="h-20"></div>
+        <div className="h-18 h-screen"></div>
         <div className="bg-nav h-full"></div>
+        
       </div>
     </main>
   );
